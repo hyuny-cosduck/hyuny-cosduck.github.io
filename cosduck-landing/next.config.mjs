f33585bd1d44b8268084d12bcb339f19a6d49f76
hyuny-cosduck <hyuny@cosduck.com>
2025-07-22 15:43:28 +0900

    Update next.config.mjs

--- cosduck-landing/next.config.mjs
+++ cosduck-landing/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',             // 🔸 정적 HTML export 설정
+  trailingSlash: true,          // 🔸 디렉토리 구조 유지 (optional)
+
   eslint: {
     ignoreDuringBuilds: true,
   },
